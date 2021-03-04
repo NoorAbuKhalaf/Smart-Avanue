8e09f5201d0f10cfb63ac4a8a91d3eb2fa74071d
NoorAbuKhalaf <noor.abukhalaf@hotmail.com>
2021-03-04 14:48:01 +0200

    Edited by Noor

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log('hiiii')
+alert("WELCOME to our page");
+
+var Age = prompt("PLEASE Enter you age"); 
+var mobilebrand = prompt("PLEASE Enter you Favorite Mobile brand"); 
+var computerbrand = prompt("PLEASE Enter you Favorite Computer brand");
+if(mobilebrand == "apple")
+{
+    alert("yes, we have apple iphones");
+} 
+else if(mobilebrand == "samsung")
+{
+    alert("yes, we have samsung mobiles");
+}
+ else 
+ {
+    alert("check what we have, welcome to our page");
+}
+
+if(computerbrand == "hp")
+{
+    alert("yes, we have hp laptops");
+} 
+else if(computerbrand == "dell")
+{
+    alert("yes, we have dell laptops");
+}
+ else 
+ {
+    alert("check what we have, welcome to our page");
+}
